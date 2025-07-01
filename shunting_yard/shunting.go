ad5759f12d5e67e57a669cf31dd8677facff84c1
cappe <159458543+cappeLindo@users.noreply.github.com>
2025-07-01 16:06:12 -0400

    Precedence 🧮 (#4)
    * ✨ new stag functions
    
    * ✨ new funcitions
    
    * feat: adding the parenthesis precedence function

---
+++ shunting_yard/shunting.go
@@ -0,0 +1,126 @@
+package shunting_yard
+
+import (
+	"stag/primitives"
+	"strconv"
+)
+
+var precedencia = map[primitives.TokenKind]int{
+	primitives.Equal:          1,
+	primitives.NotEqual:       1,
+	primitives.Less:           2,
+	primitives.LessOrEqual:    2,
+	primitives.Greater:        2,
+	primitives.GreaterOrEqual: 2,
+	primitives.Plus:           3,
+	primitives.Minus:          3,
+	primitives.Star:           4,
+	primitives.Slash:          4,
+	primitives.Carrot:         5, 
+}
+
+func isOperator(kind primitives.TokenKind) bool {
+	_, ok := precedencia[kind]
+	return ok
+}
+
+func ShuntingYard(tokens []*primitives.Token) []Statement {
+	var outputQueue []Statement
+	var operatorStack []*primitives.Token
+
+	for _, token := range tokens {
+		switch token.Kind {
+		// case primitives.Ident:
+		// outputQueue = append(outputQueue, token)
+
+		case primitives.Number:
+			n, err := strconv.Atoi(token.Literal)
+			if err != nil {
+				panic(err)
+			}
+			outputQueue = append(outputQueue, Number{Value: int64(n)})
+		
+		case primitives.OpenParen:
+			operatorStack = append(operatorStack, token)
+
+		case primitives.CloseParen:
+			for len(operatorStack) > 0 && operatorStack[len(operatorStack)-1].Kind != primitives.OpenParen {
+				top := operatorStack[len(operatorStack)-1]
+				operatorStack = operatorStack[:len(operatorStack)-1]
+
+				rhs := outputQueue[len(outputQueue)-1]
+				lhs := outputQueue[len(outputQueue)-2]
+				outputQueue = outputQueue[:len(outputQueue) -2]
+
+				switch top.Literal {
+				case "+":
+					outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: add} )
+				case "-":
+					outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: sub} )
+				case "*":
+					outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: mul} )
+				case "/": 
+					outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: div} )
+				}
+			}
+
+			if len(operatorStack) == 0 {
+				panic("Parêntese desbalanceado")
+			}
+
+			operatorStack = operatorStack[:len(operatorStack)-1]
+
+		default:
+			if isOperator(token.Kind) {
+				for len(operatorStack) > 0 {
+					top := operatorStack[len(operatorStack)-1]
+					if isOperator(top.Kind) && precedencia[top.Kind] >= precedencia[token.Kind] {
+						rhs := outputQueue[len(outputQueue)-1]
+						lhs := outputQueue[len(outputQueue)-2]
+						outputQueue = outputQueue[:len(outputQueue) -2]
+						switch top.Literal {
+						case "+":
+							outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: add} )
+						case "-":
+							outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: sub} )
+						case "*":
+							outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: mul} )
+						case "/": 
+							outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: div} )
+						}
+						
+						operatorStack = operatorStack[:len(operatorStack)-1]
+		
+					} else {
+						break
+					}
+				}
+				operatorStack = append(operatorStack, token)
+			}
+		}
+	}
+
+	for len(operatorStack) > 0 {
+		op := operatorStack[len(operatorStack)-1]
+		if op.Kind == primitives.OpenParen {
+			panic("Parêntese desbalanceado")
+		}
+		rhs := outputQueue[len(outputQueue)-1]
+		lhs := outputQueue[len(outputQueue)-2]
+		outputQueue = outputQueue[:len(outputQueue) -2]
+	
+		switch op.Literal {
+		case "+":
+			outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: add} )
+		case "-":
+			outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: sub} )
+		case "*":
+			outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: mul} )
+		case "/": 
+			outputQueue = append(outputQueue, BinaryOperation{rhs: rhs.(Expression), lhs: lhs.(Expression), op: div} )
+		}
+		operatorStack = operatorStack[:len(operatorStack)-1]
+	}
+
+	return outputQueue
+}
